79bb5228b66838bfb2b42bb944e7f23cbfbb7165
Xie Xiting <xiting.xie@gmail.com>
2016-04-27 12:25:05 +0800

    no message

--- Longest Palindromic Substring - brute force.C
+++ Longest Palindromic Substring - brute force.C
@@ -1,14 +1,14 @@
 char* longestPalindrome(char* s) {
 	
 	if(s==NULL) return NULL;
-
-    int longest = 1;
-    int N = strlen(s);
-    int i,j,m,k;
-    int start=0,end=0;
+	
+	int longest = 1;
+	int N = strlen(s);
+	int i,j,m,k;
+	int start=0,end=0;
 	
 	if(N==1||N==0) return s;
-    
+	
 	for(i=0;i<N-longest;i++){
 		j = N-1;
 		while(j-i+1>longest){
@@ -17,7 +17,7 @@ char* longestPalindrome(char* s) {
 			while(m<k){
 				if(s[m] != s[k]) break;
 				m++;--k;
-            }
+			}
 			if(m>k || m==k){
 				longest = j-i+1;
 				start = i;
@@ -27,8 +27,7 @@ char* longestPalindrome(char* s) {
 			--j;
 		}
 	}
-    
-    s[end+1] = '\0';
-    
-    return s+start;
+	
+	s[end+1] = '\0';
+	return s+start;
 }
